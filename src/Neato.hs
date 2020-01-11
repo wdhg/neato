17633a7f7b7292e9fe85718354e9fdceb6c64649
wdhg <wdhgrant@gmail.com>
2020-01-11 13:36:48 +0000

    add type synonyms

--- src/Neato.hs
+++ src/Neato.hs
@@ -1,1 +1,18 @@
 module Neato where
+
+type Neuron
+  = Int
+
+-- Gene encodes a link between two neurons, (out, in)
+type Gene
+  = (Neuron, Neuron)
+
+-- GeneHistory keeps track of genes as they appear in the population
+type GeneHistory
+  = [Gene]
+
+type Genome
+  = [(Gene, Float, Bool)]
+
+type Generation
+  = [Genome]

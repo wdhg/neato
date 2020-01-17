9c648705b2e2c088a654f3f9e86c1b7a23249840
wdhg <wdhgrant@gmail.com>
2020-01-17 17:21:41 +0000

    add base types and instances

--- src/GA/Neato.hs
+++ src/GA/Neato.hs
@@ -1,1 +1,31 @@
 module GA.Neato where
+
+type Node
+  = Int
+
+data Gene
+  = Gene Node Node Double Bool
+
+type GenePool
+  = [Gene]
+
+data Genome
+  = Genome [Gene]
+    deriving Eq
+
+instance Eq Gene where
+  (Gene in1 out1 _ _) == (Gene in2 out2 _ _)
+    = in1 == in2 && out1 == out2
+
+instance Show Gene where
+  show (Gene inNode outNode weight active)
+    = unwords
+      [ "GENE"
+      , show inNode, "-->", show outNode
+      , "(" ++ show weight
+      , show active ++ ")"
+      ]
+
+instance Show Genome where
+  show (Genome genes)
+    = unlines $ map show genes

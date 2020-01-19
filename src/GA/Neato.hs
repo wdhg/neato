b6b3ff0d7a2d895255841a2528a7dbcacf791617
wdhg <wdhgrant@gmail.com>
2020-01-19 21:52:40 +0000

    delete code

--- src/GA/Neato.hs
+++ src/GA/Neato.hs
@@ -12,7 +12,7 @@ data Gene
   = Gene Node Node Double Bool GeneID
 
 type GeneAlignment
-  = (Maybe Gene, Maybe Gene)
+  = [(Maybe Gene, Maybe Gene)]
 
 type GenePool
   = [Gene]
@@ -42,20 +42,3 @@ instance Show Gene where
 instance Show Genome where
   show (Genome genes)
     = unlines $ map show genes
-
-alignGenes :: Genome -> Genome -> GeneAlignment
-alignGenes (Genome genes1) (Genome genes2)
-  = alignGenes' (sort genes1) (sort genes2)
-    where
-      left, right :: Gene -> (Maybe Gene, Maybe Gene)
-      left gene  = (Just gene, Nothing)
-      right gene = (Nothing, Just gene)
-      alignGenes' :: [Gene] -> [Gene] -> [(Maybe Gene, Maybe Gene)]
-      alignGenes' [] genes2'
-        = map right genes2'
-      alignGenes' genes1' []
-        = map left genes1'
-      alignGenes' (gene1 : genes1') (gene2 : genes2')
-        | gene1 < gene2 = left gene1 : alignGenes' genes1' (gene2 : genes2')
-        | gene1 > gene2 = right gene2 : alignGenes' (gene1 : genes1') genes2'
-        | otherwise     = (Just gene1, Just gene2) : alignGenes' genes1' genes2'
--- test/TestNeato.hs
+++ test/TestNeato.hs
@@ -4,32 +4,15 @@ import GA.Neato
 import Test.HUnit
 import Utils
 
-alignGenesTests :: Test
-alignGenesTests
-  = equalCases (\(genes1, genes2) -> alignGenes (Genome genes1) (Genome genes2))
-    [ ( ( [gene00, gene20]
-        , [gene01, gene10]
-        )
-      , [ (Just gene00, Just gene01)
-        , (Nothing, Just gene10)
-        , (Just gene20, Nothing)
-        ])
-    , ( ( [gene00]
-        , [gene20]
-        )
-      , [ (Just gene00, Nothing)
-        , (Nothing, Just gene20)
-        ]
-      )
-    ]
-    where
-      gene00 = Gene 0 1 0.1 True 0
-      gene01 = Gene 0 1 0.6 True 0
-      gene10 = Gene 0 2 0.7 True 1
-      gene20 = Gene 0 3 0.3 False 2
+-- test genes
+gene00 = Gene 0 1 0.1 True 0
+gene01 = Gene 0 1 0.6 True 0
+gene10 = Gene 0 2 0.7 True 1
+gene20 = Gene 0 3 0.3 False 2
 
 tests :: Test
 tests
   = TestList
     [ "alignGenes" ~: alignGenesTests
+    , "splitExcess" ~: splitExcessTests
     ]

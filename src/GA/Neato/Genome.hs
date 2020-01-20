17488cc63159da373aba3b26db301ca8d7257292
wdhg <wdhgrant@gmail.com>
2020-01-20 12:24:55 +0000

    add calcMeanWeightDelta and tests

--- src/GA/Neato/Genome.hs
+++ src/GA/Neato/Genome.hs
@@ -17,7 +17,7 @@ type State
 data Gene
   = Gene Node Node Weight State GeneID
 
-type Alignment
+type Aligned
   = (Maybe Gene, Maybe Gene)
 
 type GenePool
@@ -49,14 +49,14 @@ instance Show Genome where
   show (Genome genes)
     = unlines $ map show genes
 
-alignGenes :: Genome -> Genome -> [Alignment]
+alignGenes :: Genome -> Genome -> [Aligned]
 alignGenes (Genome genes1) (Genome genes2)
   = alignGenes' (sort genes1) (sort genes2)
     where
-      left, right :: Gene -> Alignment
+      left, right :: Gene -> Aligned
       left gene  = (Just gene, Nothing)
       right gene = (Nothing, Just gene)
-      alignGenes' :: [Gene] -> [Gene] -> [Alignment]
+      alignGenes' :: [Gene] -> [Gene] -> [Aligned]
       alignGenes' [] gs2
         = map right gs2
       alignGenes' gs1' []
@@ -66,13 +66,29 @@ alignGenes (Genome genes1) (Genome genes2)
         | g1 > g2   = right g2 : alignGenes' gs1 gs2'
         | otherwise = (Just g1, Just g2) : alignGenes' gs1' gs2'
 
+calcMeanWeightDelta :: [Aligned] -> Double
+calcMeanWeightDelta alignment
+  = (sum $ map calc symmetric) / (fromIntegral $ length symmetric)
+    where
+      symmetric = filter isSymmetric alignment
+      isSymmetric :: Aligned -> Bool
+      isSymmetric (Just _, Just _)
+        = True
+      isSymmetric _
+        = False
+      calc :: Aligned -> Double
+      calc (Just (Gene _ _ weight1 _ _), Just (Gene _ _ weight2 _ _))
+        = abs $ weight1 - weight2
+
 distance :: (Double, Double, Double) -> Genome -> Genome -> Double
-distance (c1, c2, c3) (Genome genes1) (Genome genes2)
+distance (c1, c2, c3) genome1@(Genome genes1) genome2@(Genome genes2)
   = c3 * meanWeightDelta + (c1 * disjoint + c2 * excess) / n
     where
       n = fromIntegral $ max (length genes1) (length genes2)
+      alignment
+        = alignGenes genome1 genome2
       meanWeightDelta
-        = undefined
+        = calcMeanWeightDelta alignment
       disjoint
         = undefined
       excess
--- test/GenomeTest.hs
+++ test/GenomeTest.hs
@@ -7,7 +7,11 @@ import Utils
 gene00 = Gene 0 1 0.1 True 0
 gene01 = Gene 0 1 0.6 True 0
 gene10 = Gene 0 2 0.7 True 1
+gene11 = Gene 0 2 0.6 False 1
 gene20 = Gene 0 3 0.3 False 2
+gene21 = Gene 0 3 0.8 True 2
+gene30 = Gene 1 3 0.0 False 3
+gene31 = Gene 1 3 2.0 False 3
 
 alignGenesTests :: Test
 alignGenesTests
@@ -28,8 +32,27 @@ alignGenesTests
       )
     ]
 
+calcMeanWeightDeltaTests :: Test
+calcMeanWeightDeltaTests
+  = floatingCases calcMeanWeightDelta
+    [ ( [ (Just gene00, Just gene01)
+        , (Nothing, Just gene10)
+        , (Just gene20, Nothing)
+        ]
+      , 0.5
+      )
+    , ( [ (Just gene00, Nothing)
+        , (Just gene10, Just gene11)
+        , (Nothing, Just gene21)
+        , (Just gene30, Just gene31)
+        ]
+      , 1.05
+      )
+    ]
+
 tests :: Test
 tests
   = TestList
     [ "alignGenes" ~: alignGenesTests
+    , "calcMeanWeightDelta" ~: calcMeanWeightDeltaTests
     ]

b670f4610be10f372a459a25c338ac7c274a7308
wdhg <wdhgrant@gmail.com>
2020-01-20 11:53:36 +0000

    add alignGenes

--- src/GA/Neato/Genome.hs
+++ src/GA/Neato/Genome.hs
@@ -1,5 +1,7 @@
 module GA.Neato.Genome where
 
+import Data.List (sort)
+
 type Node
   = Int
 
@@ -14,6 +16,9 @@ type State
 
 data Gene
   = Gene Node Node Weight State GeneID
+
+type Alignment
+  = (Maybe Gene, Maybe Gene)
 
 type GenePool
   = [Gene]
@@ -43,6 +48,23 @@ instance Show Gene where
 instance Show Genome where
   show (Genome genes)
     = unlines $ map show genes
+
+alignGenes :: Genome -> Genome -> [Alignment]
+alignGenes (Genome genes1) (Genome genes2)
+  = alignGenes' (sort genes1) (sort genes2)
+    where
+      left, right :: Gene -> Alignment
+      left gene  = (Just gene, Nothing)
+      right gene = (Nothing, Just gene)
+      alignGenes' :: [Gene] -> [Gene] -> [Alignment]
+      alignGenes' [] gs2
+        = map right gs2
+      alignGenes' gs1' []
+        = map left gs1'
+      alignGenes' gs1@(g1 : gs1') gs2@(g2 : gs2')
+        | g1 < g2   = left g1 : alignGenes' gs1' gs2
+        | g1 > g2   = right g2 : alignGenes' gs1 gs2'
+        | otherwise = (Just g1, Just g2) : alignGenes' gs1' gs2'
 
 distance :: (Double, Double, Double) -> Genome -> Genome -> Double
 distance (c1, c2, c3) (Genome genes1) (Genome genes2)

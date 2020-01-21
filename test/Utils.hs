3c68321047eb62cf68d65a2484607153cb025e3c
wdhg <wdhgrant@gmail.com>
2020-01-21 12:36:18 +0000

    add DummyGen type

--- test/Utils.hs
+++ test/Utils.hs
@@ -1,6 +1,19 @@
 module Utils where
 
+import System.Random
 import Test.HUnit
+
+data DummyGen
+  = DummyGen Int Int [Int]
+    deriving Show
+
+instance RandomGen DummyGen where
+  next (DummyGen lower upper (value : values))
+    = (value, DummyGen lower upper values)
+  genRange (DummyGen lower upper _)
+    = (lower, upper)
+  split dummy@(DummyGen lower upper values)
+    = (dummy, DummyGen lower upper $ tail values)
 
 equalCases :: (Show b, Eq b) => (a -> b) -> [(a, b)] -> Test
 equalCases func

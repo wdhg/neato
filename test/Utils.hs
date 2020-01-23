bb323b392fa7a5e17e7afa3c3cf1e1ae01e83849
wdhg <wdhgrant@gmail.com>
2020-01-23 10:59:53 +0000

    remove DummyGen

--- test/Utils.hs
+++ test/Utils.hs
@@ -3,20 +3,6 @@ module Utils where
 import System.Random
 import Test.HUnit
 
-data DummyGen
-  = DummyGen Int Int [Int]
-    deriving Show
-
-instance RandomGen DummyGen where
-  next (DummyGen lower upper [])
-    = (0, DummyGen lower upper [])
-  next (DummyGen lower upper (value : values))
-    = (value, DummyGen lower upper values)
-  genRange (DummyGen lower upper _)
-    = (lower, upper)
-  split dummy@(DummyGen lower upper values)
-    = (dummy, DummyGen lower upper $ tail values)
-
 equalCases :: (Show b, Eq b) => (a -> b) -> [(a, b)] -> Test
 equalCases func
   = TestList . map (\(input, expected) -> func input ~?= expected)

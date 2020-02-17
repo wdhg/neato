6f235d471a03cfbe8b1c3082ecedcaeee0cb196e
wdhg <wdhgrant@gmail.com>
2020-02-17 08:03:56 +0000

    add tests for computeNode

--- test/NetworkTest.hs
+++ test/NetworkTest.hs
@@ -16,8 +16,26 @@ setNodeTests
         nodes
           = [0.0..3.0]
 
+computeNodeTests :: Test
+computeNodeTests
+  = floatingCases (\n -> (!! n) $ snd $ computeNode id (network, nodes) n)
+    [ (0, 3.2)
+    , (1, 5.7)
+    , (3, 0.6 * 5.7)
+    , (2, (3.0 * 0.6 * 5.7) + (4.0 * 3.2))
+    ]
+      where
+        network
+          = Network (2, 1)
+            [ (2, [(0,4.0), (3,3.0)])
+            , (3, [(1, 0.6)])
+            ]
+        nodes
+          = [3.2, 5.7, 0.0, 0.0]
+
 tests :: Test
 tests
   = TestList
     [ "setNode" ~: setNodeTests
+    , "computeNode" ~: computeNodeTests
     ]

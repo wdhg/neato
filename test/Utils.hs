14d09113eb3343a296e65627d361d51ecad3048b
wdhg <wdhgrant@gmail.com>
2020-03-16 20:47:20 +0000

    change floatingEqual to floatingCases

--- test/Utils.hs
+++ test/Utils.hs
@@ -2,26 +2,23 @@ module Utils (equalCases, floatingCases) where
 
 import Test.HUnit
 
-floatingEqual :: (Floating a, Ord a) => a -> a -> Bool
-floatingEqual value expected
-  = abs (value - expected) < epsilon
+floatingCase :: (Floating a, Show a, Ord a) => a -> a -> Test
+floatingCase value expected
+  = abs (value - expected) < epsilon ~? errorMessage
     where
       epsilon = 0.000001
+      errorMessage
+        = "expected: " ++ show expected ++ "\n" ++
+          " but got: " ++ show value
 
 equalCases :: (Show b, Eq b) => (a -> b) -> [(a, b)] -> Test
 equalCases func
   = TestList . map (\(input, expected) -> func input ~?= expected)
 
-floatingCases :: (Floating b, Show a, Show b, Ord b)
-              => (a -> b) -> [(a, b)] -> Test
+floatingCases :: (Floating a, Show a, Ord a)
+              => (b -> a) -> [(b, a)] -> Test
 floatingCases func
   = TestList . map testFunc
     where
       testFunc (input, expected)
-        = floatingEqual output expected ~? errorMessage
-          where
-            output
-              = func input
-            errorMessage
-              = "expected: " ++ show expected ++ "\n" ++
-                " but got: " ++ show output
+        = floatingCase (func input) expected
